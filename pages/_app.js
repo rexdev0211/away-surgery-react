a1b2204dc060d9a581023d2ad013a70c5a40da62
raorizwaniftikhar <raorizwaniftikhar@gmail.com>
2021-12-29 22:30:36 +0500

    fix jquery issue

--- pages/_app.js
+++ pages/_app.js
@@ -19,21 +19,13 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
-
         <link id="u-theme-google-font" rel="stylesheet"
           href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i|Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i" />
         <link id="u-page-google-font" rel="stylesheet"
           href="https://fonts.googleapis.com/css?family=Lato:100,100i,300,300i,400,400i,700,700i,900,900i|Poppins:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i" />
         <script src="/js/jquery.js" defer=""></script>
         <script src="/js/custompage.js" defer=""></script>
-
-        {/* <script src="/js/talkshawk.js" defer=""></script> */}
-
         <script async src="https://www.googletagmanager.com/gtag/js?id=G-2BLG34TBPY"></script>
-        <script src="/js/googleAnalytics.js" defer=""></script>
-
-
-
       </Head>
       <Component {...pageProps} />
 
